8f2c6c8f441ea989a99616e8ee9ef815bdbab18d
syedshameersarwar <syedshameersarwar@gmail.com>
2019-12-22 02:20:12 +0500

    Updating Lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,6 +3,7 @@ import { API } from "aws-amplify";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./Home.css";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
 const Home = props => {
   const [notes, setNotes] = useState([]);
@@ -50,6 +51,15 @@ const Home = props => {
     <div className='lander'>
       <h1>Scratch</h1>
       <p>A simple note taking app</p>
+
+      <div>
+        <Link to='/login' className='btn btn-info btn-lg'>
+          Login
+        </Link>
+        <Link to='/signup' className='btn btn-success btn-lg'>
+          Signup
+        </Link>
+      </div>
     </div>
   );
 
